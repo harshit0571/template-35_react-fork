47f9cad5efbbf8e2c51240e99435f307e86993c9
harshit0571 <harshitchadha65@gmail.com>
2023-11-29 20:41:35 +0530

    changes to ui

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,33 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export const Signup = ({settoggle,toggle}) => {
+export const Signup = ({ settoggle, toggle }) => {
   const [email, setemail] = useState("");
-  const [pass, setpass] = useState("")
-  const [tog, settog] = useState(false)
+  const [pass, setpass] = useState("");
+  const [tog, settog] = useState(false);
   return (
     <div class="registration-containter">
-        
-        
-    <div class="registration form">
-      <header>Signup</header>
-      <form action="#">
-        <input type="" onChange={(e)=>{setemail(e.target.value)}}  placeholder="Enter your email"/>
-        <input type="password" onChange={(e)=>{setpass(e.target.value)}}  placeholder="Create a password"/>
-        <input type="password"  placeholder="Confirm your password"/>
-        <input type="button" onClick={(e)=>{e.preventDefault(); settog(!tog)}} class="button" value="Signup"/>
-      </form>
-      {
-          tog &&
-          <div className='abbb'>
+      <div class="registration form">
+        <header>Signup</header>
+        <form action="#">
+          <input type="" placeholder="Enter your name" />
+          <input type="" placeholder="Enter your username" />
+          <input type="" placeholder="Enter your gender" />
+          <input type="" placeholder="Enter your dob" />
+          <input type="" placeholder="Enter your address" />
+          <input type="" placeholder="Enter your country" />
+          <input
+            type=""
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+            placeholder="Enter your email"
+          />
+          <input
+            type="password"
+            onChange={(e) => {
+              setpass(e.target.value);
+            }}
+            placeholder="Create a password"
+          />
+          <input type="password" placeholder="Confirm your password" />
+          <input
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              settog(!tog);
+            }}
+            class="button"
+            value="Signup"
+          />
+        </form>
+        {tog && (
+          <div className="abbb">
             <p>email: {email}</p>
             <p>password: {pass}</p>
           </div>
-         }
-          </div>
-  </div>
-
-  
-
-
-  )
-}
+        )}
+      </div>
+    </div>
+  );
+};
